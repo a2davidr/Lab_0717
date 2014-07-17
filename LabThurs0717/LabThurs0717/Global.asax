ce46b9ec3a49e62204e5bc0eefbe7a52161c4a7d
a2davidr <roedavidc@yahoo.com>
2014-07-17 15:56:10 -0400

    Original
    Original

---
+++ LabThurs0717/LabThurs0717/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LabThurs0717.MvcApplication" Language="C#" %>
---
+++ LabThurs0717/LabThurs0717/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LabThurs0717.Startup))]
+namespace LabThurs0717
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
